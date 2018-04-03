230ec52800e13f482a0ec1e3cd38422f4bb030a8
Pablo <pablo.riutort@gmail.com>
2018-04-03 13:50:29 +0200

    Virtualenv must be added to .bashrc

--- opt/python.sh
+++ opt/python.sh
@@ -1,7 +1,15 @@
-echo Installing pip
-apt-get install python-pip
+echo Installing pip3
+apt-get -y install python3-pip
+
+pip3 install virtualenv
+pip3 install virtualenvwrapper
 
 echo Installing virtualenv
-export WORKON_HOME=$HOME/.virtualenvs
-export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3
-source /usr/local/bin/virtualenvwrapper.sh
+bashrc=$HOME/.bashrc
+echo "\n" >> $bashrc
+echo export WORKON_HOME=$HOME/.virtualenvs >> $bashrc
+echo export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3 >> $bashrc
+echo source ~/.local/bin/virtualenvwrapper.sh >> $bashrc
+export $bashrc
+
+exit
