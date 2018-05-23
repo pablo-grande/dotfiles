ea6d3473981c7ac96259553005f70cec96f85e68
Pablo Riutort <pablo.riutort@gmail.com>
2018-05-23 12:53:30 +0200

    Update docker.sh
    Remove docker-compose function

--- opt/docker.sh
+++ opt/docker.sh
@@ -15,10 +15,3 @@ add-apt-repository \
 	$(lsb_release -cs) \
 	stable"
 
-
-function docker_compose {
-	echo Installing docker-compose
-	curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-	chmod +x /usr/local/bin/docker-compose
-}
-
