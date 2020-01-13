9949bdf51ad2a63d7e9c1d98d17cdebb8051e66f
Pablo <pablo.riutort@gmail.com>
2020-01-13 19:21:53 +0100

    Change exit message

--- opt/docker.sh
+++ opt/docker.sh
@@ -5,7 +5,6 @@ if [ $(id -u) -ne 0 ]; then
 	exit
 fi
 
-
 echo Downloading Docker data
 apt-get -y install \
 	apt-transport-https \
@@ -14,25 +13,19 @@ apt-get -y install \
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 
-
 echo Verifying Docker fingerprint
 apt-key fingerprint 0EBFCD88
-
 
 add-apt-repository \
 	"deb [arch=amd64] https://download.docker.com/linux/ubuntu \
 	$(lsb_release -cs) \
 	stable"
 
-
 echo Installing Docker
 apt-get -y update
 apt-get -y install docker-ce
 
-
 echo Giving permissions to $USER
 usermod -a -G docker $USER
-echo "WARNING: Log Out (or even reboot) in order to use Docker as $USER"
-
-exit
+echo "WARNING: Log Out in order to use Docker as $USER"
 
