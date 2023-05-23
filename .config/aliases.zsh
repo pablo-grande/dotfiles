1a058bff39e6c4dfaa4c6df0756b36bede41d2b0
Pablo <pablo.grande@proton.me>
2023-05-23 22:55:00 +0200

    add arch config

--- .config/aliases.zsh
+++ .config/aliases.zsh
@@ -1,5 +1,17 @@
-alias ls="ls --color=auto"
-alias la="ls -a"
-alias lt="ls --human-readable --size -1 -S --classify"
 alias ..="cd .."
-alias cleanpyc="sudo find . -type f -name '*.pyc' -delete"
+alias ls="ls --color=auto"
+alias l="ls --color=auto"
+alias la="ls -a"
+alias v="xclip -o"
+alias c="xclip"
+alias clip="c -sel clip"
+alias tsm="transmission-remote"
+alias mp="markdown_previewer"
+alias torrents="watch --interval 5 'transmission-remote -l'"
+alias jsontool="py -m json.tool"
+alias vi="vim"
+alias notebook="jupyter notebook"
+alias gpg="gpg2"
+
+# work
+alias aws='docker run --rm -ti -v ~/.aws:/root/.aws -v $(pwd):/aws amazon/aws-cli'
